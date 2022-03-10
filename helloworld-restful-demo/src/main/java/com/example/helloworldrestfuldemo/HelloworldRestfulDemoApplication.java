959ab6d6d0b812b436db2b06d217ec0a08e21bf3
Erk-Vural <er.bugrahan@gmail.com>
2022-03-10 22:05:04 +0300

    init helloworld restful demo.

---
+++ helloworld-restful-demo/src/main/java/com/example/helloworldrestfuldemo/HelloworldRestfulDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.helloworldrestfuldemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloworldRestfulDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloworldRestfulDemoApplication.class, args);
+    }
+
+}
