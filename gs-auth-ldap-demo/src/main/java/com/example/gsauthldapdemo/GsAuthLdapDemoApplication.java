1661d62d4aea3549a75454c58fef51105981a0f3
Erk-Vural <er.bugrahan@gmail.com>
2022-03-11 17:17:17 +0300

    init gs-auth-ldap-demo repo.

---
+++ gs-auth-ldap-demo/src/main/java/com/example/gsauthldapdemo/GsAuthLdapDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.gsauthldapdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsAuthLdapDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GsAuthLdapDemoApplication.class, args);
+    }
+
+}
---
+++ gs-auth-ldap-demo/src/test/java/com/example/gsauthldapdemo/GsAuthLdapDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gsauthldapdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsAuthLdapDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
